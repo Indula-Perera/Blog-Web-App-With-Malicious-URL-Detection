7618ae8dd25cb3c1466ca11a98eaa31f46c5c70e
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-10 23:45:19 +0530

    Update to Layer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
+import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'The Cyber Blog',
   description: 'The best blog app!',
 }
 
@@ -13,8 +15,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className='container'>
-          <div className='wrapper'> {children}</div>
-       
+          <div className='wrapper'> 
+              <Navbar/> 
+              {children}
+              <Footer/>
+          
+          </div>
+            
         </div>
         </body>
     </html>
