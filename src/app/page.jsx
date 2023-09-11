d11ab7596ce0c9548ba50c39150907692a6fd674
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-11 19:42:27 +0530

    add all components to page file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/CategoryList/CategoryList";
+import CardList from "@/components/CardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 
 export default function Home() {
-  return <div>
-
+  return <div className={styles.container}>
+    	<Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
   
     </div>;
 }
