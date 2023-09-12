b55168a53a1fd32e441df3f001d5375df03fe4c7
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-12 13:04:03 +0530

    toggle butten styled

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image"
 import Styles from "./themeToggle.module.css"
 
 const ThemeToggle = () => {
   return (
     <div className={Styles.container}>
-      Toggle
+      <Image src="/moon.png" alt="" width={14} height={14}/>
+      <div className={Styles.ball}></div>
+      <Image src="/sun.png" alt="" width={14} height={14}/>
     </div>
   )
 }
