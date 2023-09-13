e7c1e3cf999841d6b1c0dc5c741dd56b9de94e64
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-13 17:49:44 +0530

    create categories

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
-import styles from "./CategoryList.module.css"
+import React from "react";
+import styles from "./CategoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const CategoryList = () => {
   return (
-    <div className={styles.container}> CategoryList
-      
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Cyber}`}
+        >
+          <Image
+            src="/Cyber.jpg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Cyber Threat
+        </Link>
 
-export default CategoryList
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Ai}`}
+        >
+          <Image
+            src="/Ai.jpg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          AI
+        </Link>
+
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Chat}`}
+        >
+          <Image
+            src="/chatgpt.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Chat GPT
+        </Link>
+
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Dark}`}
+        >
+          <Image
+            src="/dark.jpg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Dark Web
+        </Link>
+
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Code}`}
+        >
+          <Image
+            src="/code.jpg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Code
+        </Link>
+
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.Virus}`}
+        >
+          <Image
+            src="/Virus.jpg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Virus
+        </Link>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
