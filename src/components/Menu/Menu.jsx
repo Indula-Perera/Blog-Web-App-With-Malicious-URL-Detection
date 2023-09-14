4ca4744bf1eab8427b55268cdedbd8563b1ba2c5
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-14 19:00:01 +0530

    Start Create Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from "./Menu.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 const Menu = () => {
   return (
-    <div className={styles.container}> Menu
+    <div className={styles.container}> 
+    <h2>{"What's hot "}</h2>
+    <h1> Most Popular</h1>
+    <div className={styles.items}>
+      <Link href="/" className={styles.item}></Link>
+      <div className={styles.imageContainer}>
+        <Image src="/mail.jpg" alt="" fill className={styles.image}/>
+      </div>
+      <div className={styles.textContainer}>
+        <span className={`${styles.category} ${styles.Cyber}`}> Cyber Threat </span>
+        
+      </div>
+    </div>
     </div>
   )
 }
