c19a298f9e832ad1137a4dbc51398a5a6b9a34cf
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-15 01:19:43 +0530

    footer part done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import style from "./Footer.module.css"
+import React from "react";
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={style.container}>
-      Footer
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo1.png" alt="lama blog" width={50} height={50} className={styles.logoimg} />
+          <h1 className={styles.logoText}>The Cyber Blog </h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
+          porro sequi, totam minima consequuntur, aspernatur deleniti vero
+          repellendus dorales.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Home</Link>
+          <Link href="/">Check URL</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Cyber Threat</Link>
+          <Link href="/">Chat GPT</Link>
+          <Link href="/">Dark Web</Link>
+          <Link href="/">Coding</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
 
-export default Footer
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
