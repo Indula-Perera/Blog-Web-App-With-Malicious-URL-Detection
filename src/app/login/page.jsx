d1027bf9a47746a4fe5a3a824230890214348da0
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-19 22:25:51 +0530

    add icons to login options

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from './loginPage.module.css'
 
 const LoginPage = () => {
@@ -5,9 +6,9 @@ const LoginPage = () => {
     <div className={styles.container}>
        <div className={styles.wrapper}>
 
-        <div className={styles.socialButton}> Sign in width Google </div>
-        <div className={styles.socialButton}> Sign in width Github </div>
-        <div className={styles.socialButton}> Sign in width Facebook </div>
+        <div className={styles.socialButton}> <Image src="/google.png" alt='' width={20} height={20}/> Sign in width Google </div>
+        <div className={styles.socialButton}> <Image src="/github.png" alt='' width={20} height={20}/>Sign in width Github </div>
+        <div className={styles.socialButton}> <Image src="/facebook.png" alt='' width={20} height={20}/>Sign in width Facebook </div>
       
       </div>
     </div>
