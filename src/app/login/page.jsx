f1b5b8af923d202fa2cc0e015fe9da92c9aebd65
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-19 22:27:59 +0530

    add class for images

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,9 +6,9 @@ const LoginPage = () => {
     <div className={styles.container}>
        <div className={styles.wrapper}>
 
-        <div className={styles.socialButton}> <Image src="/google.png" alt='' width={20} height={20}/> Sign in width Google </div>
-        <div className={styles.socialButton}> <Image src="/github.png" alt='' width={20} height={20}/>Sign in width Github </div>
-        <div className={styles.socialButton}> <Image src="/facebook.png" alt='' width={20} height={20}/>Sign in width Facebook </div>
+        <div className={styles.socialButton}> <Image src="/google.png" alt='' width={30} height={30} className={styles.img}/> Sign in with Google </div>
+        <div className={styles.socialButton}> <Image src="/github.png" alt='' width={30} height={30} className={styles.img}/>Sign in with Github </div>
+        <div className={styles.socialButton}> <Image src="/facebook.png" alt='' width={30} height={30} className={styles.img}/>Sign in with Facebook </div>
       
       </div>
     </div>
