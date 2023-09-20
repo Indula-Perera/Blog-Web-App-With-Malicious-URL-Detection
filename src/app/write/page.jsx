9c518f2df2adbf559e98f39342c88e512c7622f9
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-20 13:24:28 +0530

    install react-quill

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './write.module.css'
+import { useState } from 'react'
+import ReactQuill from 'react-quill'
+import "react-quill/dist/quil.bubble.css"
 
 const WritePage = () => {
+
+    const [open,setOpen] = useState(false)
+    const [value,setValue] = useState("")
   return (
     <div className={styles.container}>
        <input type='text' placeholder='Title' />
@@ -9,6 +17,21 @@ const WritePage = () => {
         <button className={styles.button}>
           <Image src="/plus.png" alt='' width={16} height={16}/>
         </button>
+        {open &&(
+          <div className={styles.add}>
+             <button className={styles.addButton}>
+          <Image src="/image.png" alt='' width={16} height={16}/>
+        </button> 
+        <button className={styles.addButton}>
+          <Image src="/external.png" alt='' width={16} height={16}/>
+        </button> 
+        <button className={styles.addButton}>
+          <Image src="/video.png" alt='' width={16} height={16}/>
+        </button>
+          </div>
+        )}
+
+        <ReactQuill theme='bubble'/>
        </div>
     </div>
   )
