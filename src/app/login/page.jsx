98fe8b69b08b1c649379ca8891dca1aaf8e4d19c
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-22 02:12:23 +0530

    first step to Signin Google

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,20 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './loginPage.module.css'
+import { signIn, useSession } from 'next-auth/react'
 
 const LoginPage = () => {
+
+  const {data, status} = useSession();
+  
+  console.log(data, status)
+
   return (
     <div className={styles.container}>
        <div className={styles.wrapper}>
 
-        <div className={styles.socialButton}> <Image src="/google.png" alt='' width={30} height={30} className={styles.img}/> Sign in with Google </div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}> <Image src="/google.png" alt='' width={30} height={30} className={styles.img}/> Sign in with Google </div>
         <div className={styles.socialButton}> <Image src="/github.png" alt='' width={30} height={30} className={styles.img}/>Sign in with Github </div>
         <div className={styles.socialButton}> <Image src="/facebook.png" alt='' width={30} height={30} className={styles.img}/>Sign in with Facebook </div>
       
