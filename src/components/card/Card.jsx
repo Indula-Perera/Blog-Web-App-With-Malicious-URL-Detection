11108a8ff45feb6a94f46b8a68aadcc700df3b44
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2023-09-28 17:29:11 +0530

    create new slugs

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,29 +5,27 @@ import Link from "next/link";
 const card = ({key, item}) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src="/mail.jpg" alt="" fill className={styles.image} />
-      </div>
-
+      {item.img &&
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+       
+      }
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}> {item.createdAt.substring(0,10)} - {""}</span>
+          <span className={styles.date}> 
+          {item.createdAt.substring(0,10)} - {""}</span>
           <span className={styles.category}> {item.catSlug}</span>
         </div>
-        <Link href="/">
+        <Link href={`/posts/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
         <p className={styles.desc}>
           
-          When a product, that has the word &lsquo;Security&apos; in its name,
-          becomes the target of a zero-day attack which ends up compromising
-          several other businesses, it is certainly an event to be studied and
-          learn from. Hence, we created this timeline that covers everything
-          that happened in the Barracuda Email Security Gateway Appliances
-          Attack.
+         {item.desc.substring(0,60)}
 
         </p>
-        <Link href="/" className={styles.link}>Read More</Link>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
       </div>
     </div>
   );
